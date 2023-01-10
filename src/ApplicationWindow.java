0f9c7ec20e5a0d1231d53af85a13aac1cc8d8e2c
koziu <kaxpiq@gmail.com>
2023-01-10 13:00:44 +0100

    Sekwencja białkowa (nie działa)

--- src/ApplicationWindow.java
+++ src/ApplicationWindow.java
@@ -9,6 +9,7 @@ public class ApplicationWindow extends JFrame {
     private JLabel label1;
     private JLabel label2;
     private JLabel label3;
+    private JLabel label4;
 
 
     public ApplicationWindow() {
@@ -27,9 +28,116 @@ public class ApplicationWindow extends JFrame {
             displacementTwo.toUpperCase();
             displacementThree.toUpperCase();
 
-            label1.setText("+1: "+displacementOne.replaceAll("(.{" + 3 + "})", "$1 ").trim());
-            label2.setText("+2: "+displacementTwo.substring(1).replaceAll("(.{" + 3 + "})", "$1 ").trim());
-            label3.setText("+3: "+displacementThree.substring(2).replaceAll("(.{" + 3 + "})", "$1 ").trim());
+            //label1.setText("+1: " + displacementOne.replaceAll("(.{" + 3 + "})", "$1 ").trim());
+            //label2.setText("+2: " + displacementTwo.substring(1).replaceAll("(.{" + 3 + "})", "$1 ").trim());
+            //label3.setText("+3: " + displacementThree.substring(2).replaceAll("(.{" + 3 + "})", "$1 ").trim());
+
+
+            String proteinSequence = "";
+
+            for (int i = 0; i < displacementOne.length(); i += 3) {
+                String currentCodon = displacementOne.substring(i, i + 3);
+                switch (currentCodon) {
+                    case "UUU":
+                    case "UUC":
+                        proteinSequence += "F";
+                        break;
+                    case "UUA":
+                    case "UUG":
+                    case "CUU":
+                    case "CUC":
+                    case "CUA":
+                    case "CUG":
+                        proteinSequence += "L";
+                        break;
+                    case "AUU":
+                    case "AUC":
+                    case "AUA":
+                        proteinSequence += "I";
+                        break;
+                    case "AUG":
+                        proteinSequence += "M";
+                        break;
+                    case "GUU":
+                    case "GUC":
+                    case "GUA":
+                    case "GUG":
+                        proteinSequence += "V";
+                        break;
+                    case "UCU":
+                    case "UCC":
+                    case "UCA":
+                    case "UCG":
+                    case "AGU":
+                    case "AGC":
+                        proteinSequence += "S";
+                        break;
+                    case "CCU":
+                    case "CCC":
+                    case "CCA":
+                    case "CCG":
+                        proteinSequence += "P";
+                        break;
+                    case "ACU":
+                    case "ACC":
+                    case "ACA":
+                    case "ACG":
+                        proteinSequence += "T";
+                        break;
+                    case "GCU":
+                    case "GCC":
+                    case "GCA":
+                    case "GCG":
+                        proteinSequence += "A";
+                        break;
+                    case "UAU":
+                    case "UAC":
+                        proteinSequence += "Y";
+                        break;
+                    case "CAU":
+                    case "CAC":
+                        proteinSequence += "H";
+                        break;
+                    case "CAA":
+                    case "CAG":
+                        proteinSequence += "Q";
+                        break;
+                    case "AAU":
+                    case "AAC":
+                        proteinSequence += "N";
+                        break;
+                    case "AAA":
+                    case "AAG":
+                        proteinSequence += "K";
+                        break;
+                    case "GAU":
+                    case "GAC":
+                        proteinSequence += "D";
+                        break;
+                    case "GAA":
+                    case "GAG":
+                        proteinSequence += "E";
+                        break;
+                    case "UGU":
+                    case "UGC":
+                        proteinSequence += "C";
+                        break;
+                    case "UGG":
+                        proteinSequence += "W";
+                        break;
+                    case "CGU":
+                    case "CGC":
+                    case "CGA":
+                    case "CGG":
+                    case "AGA":
+                    case "AGG":
+                        proteinSequence += "R";
+                        break;
+                    default:
+                        proteinSequence = "error";
+                };
+            }
+            label4.setText(proteinSequence);
         }
     });
 }
